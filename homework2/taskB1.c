6b9424b0391a460f5eea63774ded49c59c5193d8
Natalia <nataliazanegina@yandex.ru>
2025-11-01 01:08:16 +0300

    homework 2 done

--- homework2/taskB1.c
+++ homework2/taskB1.c
@@ -1,3 +1,30 @@
+/*
+Всего памяти
+
+Описана структура данных для хранения информации об использованной памяти:
+typedef struct list {
+void *address;
+size_t size;
+char comment[64];
+struct list *next;
+} list;
+
+Требуется реализовать только одну функцию, которая анализирует данный список и возвращает сколько всего памяти 
+используется. Адрес хранится в поле address, поле size - соответствующий размер данного блока. Если список пустой,
+то функция должна возвращать 0.
+
+Прототип функции:
+size_t totalMemoryUsage(list *head)
+
+Examples
+Input
+3
+	  140525067852320 10
+	  140525067852350 30
+	  140525067852900 100
+Output
+140
+*/
 #include <stdio.h>
 #include <stdint.h>
 
@@ -11,11 +38,11 @@ typedef struct list
 
 size_t totalMemoryUsage(list *head)
 {
-    
-}
+    size_t res = 0;
 
-int main(void)
-{
-    /* code */
-    return 0;
+    for (list *b = head; b != NULL; b = b->next)
+        res += b->size;
+    
+    return res;
+    
 }
